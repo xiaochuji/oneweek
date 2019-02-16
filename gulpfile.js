36e4dda6cf20102fef1c9e756859b62803156e00
zhenglei <1158464239@qq.com>
2019-02-16 09:37:03 +0800

    压缩sass及排版

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,18 @@ var ug = require("gulp-uglify");
 var server = require("gulp-webserver");
 
 //编译sass
-
+gulp.task("sass",function(){
+    return gulp.src("./src/scss/*.scss")
+           .pipe(sass())
+           .pipe(gulp.dest("./src/css"))
+})
 
 //压缩sass
-
+gulp.task("concat",function(){
+    return gulp.src("./src/css/*.css")
+           .pipe(concat("all.css"))
+           .pipe(gulp.dest("./src/css"))
+})
 
 //起服务
 
@@ -17,5 +25,9 @@ var server = require("gulp-webserver");
 //压缩js
 
 //监听
+gulp.task("watch",function(){
+    gulp.watch("./src/scss/*.scss",gulp.series("sass","concat"))
+})
 
+gulp.task("all",gulp.series("sass","concat","watch"))
 //生成dist文件夹
