86681ed8318dfea9363e12a3c07540fb5279e1e4
zhenglei <1158464239@qq.com>
2019-02-16 09:14:37 +0800

    阻止架构

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var concat = require("gulp-concat");
+var clean = require("gulp-cleancss");
+var ug = require("gulp-uglify");
+var server = require("gulp-webserver");
+
+//编译sass
+
+
+//压缩sass
+
+
+//起服务
+
+
+//压缩js
+
+//监听
+
+//生成dist文件夹
